db3931e8c76b4352fafa10877de29e4979f2f752
Alexandre Piel <alexandre.piel@gmail.com>
2017-12-21 20:28:03 +0100

    now working with both solution

--- httpd.cpp
+++ httpd.cpp
@@ -1,4 +1,5 @@
-#include <lwip/api.h>
+#include <lwip/api.h> // maybe dont need anymore when switch to tcp
+#include <lwip/tcp.h>
 #include <string.h>
 #include <espressif/esp_common.h>
 
@@ -81,8 +82,8 @@ char * parse_request(char *data)
 {
     char * response = NULL;
 
-    printf("Received data:\n%s\n", (char*) data);
     if (!strncmp(data, "GET ", 4) || !strncmp(data, "PUT ", 4)) { // is this really necessary?
+        printf("Received data:\n%s\n", (char*) data);
         char uri[128];
         str_extract(data, '/', ' ', uri);
         printf("uri: %s\n", uri);
@@ -160,75 +161,216 @@ char * ws_get_response(char * data)
   return buf;
 }
 
+// static void ICACHE_FLASH_ATTR parseWsFrame(char *data, WSFrame *frame) {
+// 	frame->flags = (*data) & FLAGS_MASK;
+// 	frame->opcode = (*data) & OPCODE_MASK;
+// 	//next byte
+// 	data += 1;
+// 	frame->isMasked = (*data) & IS_MASKED;
+// 	frame->payloadLength = (*data) & PAYLOAD_MASK;
+
+// 	//next byte
+// 	data += 1;
+
+// 	if (frame->payloadLength == 126) {
+// 		os_memcpy(&frame->payloadLength, data, sizeof(uint16_t));
+// 		data += sizeof(uint16_t);
+// 	} else if (frame->payloadLength == 127) {
+// 		os_memcpy(&frame->payloadLength, data, sizeof(uint64_t));
+// 		data += sizeof(uint64_t);
+// 	}
+
+// 	if (frame->isMasked) {
+// 		os_memcpy(&frame->maskingKey, data, sizeof(uint32_t));
+// 		data += sizeof(uint32_t);
+// 	}
+
+// 	frame->payloadData = data;
+// }
+
 void ws_read(u8_t * data)
 {
-    int data_offset = 6;
-    u8_t *dptr = &data[6];
-    u8_t *kptr = &data[2];
-    u16_t len = data[1] & 0x7F;
+    uint8_t opcode = (*data) & 0x0F;
+    data += 1;
+    uint8_t isMasked = (*data) & (1<<7);
+    uint32_t len = (*data) & 0x7F;
+    data += 1;
+    printf("opcode %d len %d ismasked %d\n", opcode, len, isMasked);
 
-    if (len == 127) {
-        return; // ERR_OK
-    } else if (len == 126) {
-        /* extended length */
-        dptr += 2;
-        kptr += 2;
-        data_offset += 2;
-        len = (data[2] << 8) | data[3];
+	if (len == 126) {
+		memcpy(&len, data, sizeof(uint16_t));
+		data += sizeof(uint16_t);
+	} else if (len == 127) {
+		memcpy(&len, data, sizeof(uint64_t));
+		data += sizeof(uint64_t);
+	}
+
+	if (isMasked) {
+        uint32_t maskingKey;
+		memcpy(&maskingKey, data, sizeof(uint32_t));
+		data += sizeof(uint32_t);
+        for (uint32_t i = 0; i < len; i++) {
+            int j = i % 4;
+            data[i] = data[i] ^ ((uint8_t *)&maskingKey)[j];
+        }
+        printf("ws data: %s\n", data);
+	} else {
+        printf("ws we should close connexion... masked...\n");
+    }
+}
+
+static err_t http_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
+{
+//   err_t parsed = ERR_ABRT;
+//   struct http_state *hs = (struct http_state *)arg;
+
+  u8_t *data = (u8_t *) p->payload;
+  u16_t data_len = p->len;
+
+  printf("rcv data: %s\n", data);
+
+    char * response = NULL;
+    if (wsOpen) {
+        ws_read((u8_t *)data);
+    } else {
+        printf("read request\n");
+        response = parse_request((char *)data);
     }
 
-    /* unmask */
-    for (int i = 0; i < len; i++) {
-        *(dptr++) ^= kptr[i % 4];
+    if (!response) {
+        if (wsOpen) {
+            printf("ws: send default response\n");
+            response = ws_get_response("hello\n");                         
+        } else {
+            printf("httpd: send default response\n");
+            response = httpd_get_default_response();                         
+        }
     }
+    if (response) {
+        printf("write response\n");
+        // netconn_write(client, response, strlen(response), NETCONN_COPY);
+        err_t err = tcp_write(pcb, response, strlen(response), 0);
+        if (err != ERR_OK) {
+            printf("Something went wrong while tcp write\n");
+        }
+        printf("write done\n");
+    }
+    if (!wsOpen) {
+        printf("break\n");
+        // break;
+    }
+    printf("wait for message\n");
 
-    printf("ws read: %s\n", data);
+  pbuf_free(p);
+  return ERR_OK;
+}
+
+static err_t http_accept(void *arg, struct tcp_pcb *pcb, err_t err)
+{
+    struct tcp_pcb_listen *lpcb = (struct tcp_pcb_listen*)arg;
+
+    printf("http_accept %p / %p\n", (void*)pcb, arg);  
+
+    /* Decrease the listen backlog counter */
+    tcp_accepted(lpcb);
+    /* Set priority */
+    tcp_setprio(pcb, TCP_PRIO_MIN);    
+ 
+    /* Set up the various callback functions */
+    tcp_recv(pcb, http_recv);
+    // tcp_err(pcb, http_err);
+    // tcp_poll(pcb, http_poll, HTTPD_POLL_INTERVAL);
+    // tcp_sent(pcb, http_sent);
+
+    return ERR_OK;
 }
 
 void httpd_task(void *pvParameters)
 {
-    struct netconn *client = NULL;
-    struct netconn *nc = netconn_new(NETCONN_TCP);
-    if (nc == NULL) {
-        printf("Failed to allocate socket\n");
+    struct tcp_pcb *pcb;
+    err_t err;
+
+    pcb = tcp_new();
+    if (pcb == NULL) {
+        printf("httpd_init: tcp_new failed\n");
         vTaskDelete(NULL);
     }
-    netconn_bind(nc, IP_ADDR_ANY, HTTPD_PORT);
-    netconn_listen(nc);
-    char * response = NULL;
-    while (1) {
-        err_t err = netconn_accept(nc, &client);
-        if (err == ERR_OK) {
-            struct netbuf *nb;
-            wsOpen = false;
-            while ((err = netconn_recv(client, &nb)) == ERR_OK) {
-                void *data = NULL;
-                u16_t len;
-                if (netbuf_data(nb, &data, &len) == ERR_OK) {
-                    // printf("Received data:\n%.*s\n", len, (char*) data);
-                    response = parse_request((char *)data);
-                    if (!response) {
-                        ws_read((u8_t *)data);
-                    }                         
-                }          
-                if (!response) {
-                    if (wsOpen) {
-                        printf("ws: send default response\n");
-                        response = ws_get_response("hello\n");                         
-                    } else {
-                        printf("httpd: send default response\n");
-                        response = httpd_get_default_response();                         
-                    }
-                }
-                netconn_write(client, response, strlen(response), NETCONN_COPY);
-                if (!wsOpen) {
-                    break;
-                }
-            }
-            netbuf_delete(nb);
-        }
-        // printf("Closing connection\n");
-        netconn_close(client);
-        netconn_delete(client);
+    tcp_setprio(pcb, TCP_PRIO_MIN);
+    // /* set SOF_REUSEADDR here to explicitly bind httpd to multiple interfaces */
+    err = tcp_bind(pcb, IP_ADDR_ANY, HTTPD_PORT);
+    if (err != ERR_OK) {
+        printf("httpd_init: tcp_bind failed\n");
+        vTaskDelete(NULL);
+    }   
+    pcb = tcp_listen(pcb);
+    if (pcb == NULL) {
+        printf("httpd_init: tcp_listen failed\n");
+        vTaskDelete(NULL);
+    }    
+    // /* initialize callback arg and accept callback */
+    tcp_arg(pcb, pcb);
+    tcp_accept(pcb, http_accept);
+
+    // ----- end
+
+    while(1) {
+        vTaskDelay(1000);
     }
+
+    // struct netconn *client = NULL;
+    // struct netconn *nc = netconn_new(NETCONN_TCP);
+    // if (nc == NULL) {
+    //     printf("Failed to allocate socket\n");
+    //     vTaskDelete(NULL);
+    // }
+    // netconn_bind(nc, IP_ADDR_ANY, HTTPD_PORT);
+    // netconn_listen(nc);
+    // while (1) {
+    //     err_t err = netconn_accept(nc, &client);
+    //     if (err == ERR_OK) {
+    //         struct netbuf *nb;
+    //         wsOpen = false;
+    //         while ((err = netconn_recv(client, &nb)) == ERR_OK) {
+    //             printf("recv\n");
+    //             do {
+    //                 void *data = NULL;
+    //                 char * response = NULL;
+    //                 u16_t len;
+    //                 if (netbuf_data(nb, &data, &len) == ERR_OK) {
+    //                     // printf("Received data:\n%.*s\n", len, (char*) data);
+    //                     if (wsOpen) {
+    //                         ws_read((u8_t *)data);
+    //                     } else {
+    //                         printf("read request\n");
+    //                         response = parse_request((char *)data);
+    //                     }
+    //                 }          
+    //                 if (!response) {
+    //                     if (wsOpen) {
+    //                         printf("ws: send default response\n");
+    //                         response = ws_get_response("hello\n");                         
+    //                     } else {
+    //                         printf("httpd: send default response\n");
+    //                         response = httpd_get_default_response();                         
+    //                     }
+    //                 }
+    //                 if (response) {
+    //                     printf("write response\n");
+    //                     netconn_write(client, response, strlen(response), NETCONN_COPY);
+    //                     printf("write done\n");
+    //                 }
+    //             } while (netbuf_next(nb) >= 0);
+    //             netbuf_delete(nb);
+    //             if (!wsOpen) {
+    //                 break;
+    //             }    
+    //             printf("wait for message\n");
+    //         }
+    //         printf("nb del\n");
+    //         netbuf_delete(nb);
+    //     }
+    //     // printf("Closing connection\n");
+    //     netconn_close(client);
+    //     netconn_delete(client);
+    // }
 }
