502b14adb520b3b0ab543645e646ce6e2fc0a5da
Alexandre Piel <alexandre.piel@gmail.com>
2018-03-11 21:43:40 +0100

    execute timer

--- test/timer.cpp
+++ test/timer.cpp
@@ -1,12 +1,35 @@
 #include "../timer.cpp"
 #include <gtest/gtest.h>
 
+int hello_called = 0;
 void hello() {
+	hello_called++;
+}
 
+void clear_timer() {
+	for(int pos = 0; pos < TIMER_SIZE; pos++) {
+		timer[pos].time = 0;
+	}
+}
+
+void full_timer() {
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);
 }
 
 TEST(add_timer, add)
 {
+	clear_timer();
+
 	unsigned long current_time = sdk_system_get_time();
 	ASSERT_EQ(0, add_timer(hello, 10));
 	ASSERT_EQ(current_time + 10 * 1000000, timer[0].time);
@@ -21,16 +44,43 @@ TEST(add_timer, add)
 	ASSERT_EQ(hello, timer[2].callback);
 }
 
+TEST(add_timer, add_full)
+{
+	clear_timer();
+	full_timer();
+	ASSERT_EQ(-1, add_timer(hello, 10));
+}
+
 TEST(get_free_timer, get)
 {
-	ASSERT_EQ(3, get_free_timer());
-	add_timer(hello, 10);
-	ASSERT_EQ(4, get_free_timer());
-	add_timer(hello, 10);
-	add_timer(hello, 10);
-	add_timer(hello, 10);
-	add_timer(hello, 10);
-	add_timer(hello, 10);
-	add_timer(hello, 10);
+	clear_timer();
+	ASSERT_EQ(0, get_free_timer());
+	full_timer();
 	ASSERT_EQ(-1, get_free_timer());
+	sdk_current_time += 50 * 1000000;
+	ASSERT_EQ(0, get_free_timer());
+	add_timer(hello, 10);
+	ASSERT_EQ(1, get_free_timer());
+}
+
+TEST(execute_timer, execute)
+{
+	clear_timer();
+	hello_called = 0;
+	add_timer(hello, 10);
+	execute_timer();
+	ASSERT_EQ(0, hello_called);
+	sdk_current_time += 50 * 1000000;
+	execute_timer();
+	ASSERT_EQ(1, hello_called);
+
+	hello_called = 0;
+	add_timer(hello, 10);
+	add_timer(hello, 10);
+	add_timer(hello, 10);	
+	execute_timer();
+	ASSERT_EQ(0, hello_called);
+	sdk_current_time += 50 * 1000000;
+	execute_timer();
+	ASSERT_EQ(3, hello_called);
 }
--- timer.cpp
+++ timer.cpp
@@ -24,7 +24,21 @@ int get_free_timer() {
 int add_timer(void (*callback)(void), int seconds)
 {
     int pos = get_free_timer();
-    timer[pos].time = sdk_system_get_time() + (seconds * 1000000);
-    timer[pos].callback = callback;
+    if (pos > -1) {
+        timer[pos].time = sdk_system_get_time() + (seconds * 1000000);
+        timer[pos].callback = callback;
+    }
     return pos;
+}
+
+void execute_timer()
+{
+    int pos = 0;
+    unsigned long current_time = sdk_system_get_time();
+    for(; pos < TIMER_SIZE; pos++) {
+        if (timer[pos].time > 0 && timer[pos].time < current_time) {
+            timer[pos].callback();
+            timer[pos].time = 0;
+        }
+    }
 }
